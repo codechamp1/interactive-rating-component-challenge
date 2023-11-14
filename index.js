223ebd66b8a5ffff42243f89351b05594c438c5d
codechamp1 <mihaitanase20@gmail.com>
2023-11-14 12:47:46 +0100

    Added animations

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-
-const selectRating = (rating) => {
-    localStorage.setItem('selectedRating', rating);
-}
+const selectRating = rating => {
+  localStorage.setItem("selectedRating", rating);
+  const submitBtn = document.getElementById("submitBtn");
+  submitBtn.disabled = false;
+  submitBtn.classList.add("animated");
+};
 
 const submitRating = () => {
-    document.getElementById('selectedRating').innerHTML = localStorage.getItem('selectedRating');
-    document.getElementById('frontCard').classList.add("hide")
-    document.getElementById('backCard').classList.remove("hide")
-}
-
+  document.getElementById("card").classList.add("flip");
+  document.getElementById("selectedRating").innerHTML =
+    localStorage.getItem("selectedRating");
+  document.getElementById("frontCard").classList.add("hide");
+  document.getElementById("backCard").classList.remove("hide");
+};
